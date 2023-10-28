ad8c4f0f0e2167f0cc47b1403f9fd3406541bacb
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-10-28 15:25:18 -0300

    unigram.r

--- unigram.r
+++ unigram.r
@@ -1,10 +1,13 @@
 library(udpipe)
+library(readr)
 
 model <- udpipe_download_model(language = "portuguese")
 model_path <- model$file_model
 model_loaded <- udpipe_load_model(file = model_path)
 
-x <- udpipe_annotate(model_loaded, x = "Ora, vá pelo caminho das flores, menina!", tagger = "default", parser = "none")
+corpora <- read_file("C:\\Users\\User1\\Documents\\corpora.txt")
+
+x <- udpipe_annotate(model_loaded, x = corpora, tagger = "default", parser = "none")
 x <- as.data.frame(x)
 
 data <- data.frame(
