f7ca02ec698ebabe4ed3a50de9c3b228e4c9db6b
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-10-28 16:10:42 -0300

    unigram.r

--- n-grams/unigram.r
+++ n-grams/unigram.r
@@ -6,7 +6,7 @@ model <- udpipe_download_model(language = "portuguese")
 model_path <- model$file_model
 model_loaded <- udpipe_load_model(file = model_path)
 
-corpora <- read_file("corpora.txt")
+corpora <- read_file("corpus/corpora1.txt")
 
 x <- udpipe_annotate(model_loaded, x = corpora, tagger = "default", parser = "none")
 x <- as.data.frame(x)
